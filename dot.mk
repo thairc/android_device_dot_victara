0f45120c67bf076733e2734c7e1dea0cf3dda5b9
ThaiRC <thainguyenn67@gmail.com>
2017-12-09 04:14:11 -0800

    Add files via upload

---
+++ dot.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/motorola/victara/full_victara.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_RELEASE_NAME := MOTO X (2014)
+PRODUCT_NAME := dot_victara
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
